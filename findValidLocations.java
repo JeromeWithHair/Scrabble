cba4d51ed7f61b3a0f1fa174492247d72ef78896
JeromeWithHair <45410679+JeromeWithHair@users.noreply.github.com>
2018-12-05 14:49:31 -0800

    Create findValidLocations.java
    Created some psudocode to look for valid locations

---
+++ findValidLocations.java
@@ -0,0 +1,23 @@
+//This method will go through the board to find good locations to place a word
+
+public int[][] findValidLocations(Board b){
+for(int i = 0; i<15; i++){
+  for(int j = 0; j<15; j++){
+  if(b.getSquare(new Location(i,j)) != isAlphabetical //or other symbols){
+    // Square is occupied
+    return 0;
+    }
+    else{//check path 
+    
+    
+    if(// no neighbors within 7)
+     return 0;
+     }
+     else{
+     return path length
+
+
+
+
+}
+
