391c19ce1afd9b7cb8417f793a0932707f3c4881
JeromeWithHair <45410679+JeromeWithHair@users.noreply.github.com>
2018-12-05 22:28:55 -0800

    Working Test File of Finding Valid Locations
    I updated the method within a bit too. Will copy paste as needed

---
+++ testingValidLocation.java
@@ -0,0 +1,91 @@
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class testingValidLocation {
+
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        b.placeWord("kiss", Location.CENTER, Location.VERTICAL);
+        b.placeWord("aws", new Location(9,8), Location.HORIZONTAL);
+        int[][][] lengths = findValidLocations(b);
+        for(int i = 0; i< 15; i++){
+            StdOut.println();
+            for(int j = 0; j<15; j++){
+                StdOut.print(i + "," + j + ": " + Arrays.toString(lengths[i][j]) + "  ");
+            }
+        }
+    }
+
+    public static int[][][] findValidLocations(Board b){
+        int[][][] lengths = new int[15][15][4];
+        for(int i = 0; i<15; i++){
+            for(int j = 0; j<15; j++){
+                Location current = new Location(i,j);
+                if(b.isOccupied(current)){
+                    for(int k = 0; k<4; k++){
+                        lengths[i][j][k] = 0;
+                    }
+                }
+                else{//check path
+                    int[] check;
+                    check = findEmptyPath(b,current,Location.HORIZONTAL);
+                    lengths[i][j][0] = check[0];
+                    lengths[i][j][1] = check[1];
+                    check = findEmptyPath(b,current,Location.VERTICAL);
+                    lengths[i][j][2] = check[0];
+                    lengths[i][j][3] = check[1];
+                }
+            }
+        }
+        return lengths;
+    }
+    public static int[] findEmptyPath(Board b, Location start, Location direction){
+        int min = -1; // minimum length needed to play off
+        int index = 0;
+        Location current = start;
+        while(index<8){
+            if(!current.isOnBoard()){
+                if(min == -1){
+                    index = 8;
+                    break;
+                }
+                else{
+                    break;
+                }
+            }
+            if(b.isOccupied(current)){ // found a tile
+                break;
+            }
+            Location neighbor1 = current.neighbor(direction.opposite());
+            Location antineighbor2 = current.antineighbor(direction.opposite());
+            if(index == 0 && current.antineighbor(direction).isOnBoard() && b.isOccupied(current.antineighbor(direction))){
+                min = 1;
+            }
+            boolean check1 = neighbor1.isOnBoard() && b.isOccupied(neighbor1);
+            boolean check2 = antineighbor2.isOnBoard() && b.isOccupied(antineighbor2);
+            if(min == -1 && (check1 || check2)){
+                min = index+1;
+                if(min == 8) { min = -1;}
+            }
+            index++;
+            current = current.neighbor(direction);
+        }
+        int[] values = new int[2];
+        if(min == -1 && index == 8){ //cannot play off
+            values[0] = 0;
+            values[1] = 0;
+        }
+        else if (min == -1 && index < 8){ // ends by ramming into a word, but nothing before then
+            values[0] = index;
+            values[1] = index;
+        }
+        else{
+            if(index == 8){ index = 7;}
+            values[0] = index;
+            values[1] = min;
+        }
+        return values;
+    }
+}
