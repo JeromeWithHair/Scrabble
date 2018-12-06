41ad14790c49a2c7330260377df9cf748ed63f4d
JeromeWithHair <45410679+JeromeWithHair@users.noreply.github.com>
2018-12-05 18:38:38 -0800

    Added final meat to this method
    This method will look at the board then update each tile with this information:
    1: Max word length in Horizontal
    2: Min word length in Horizontal
    3: Max word length in Vertical 
    4: Min word length in Vertical

--- findValidLocations.java
+++ findValidLocations.java
@@ -1,23 +1,55 @@
 //This method will go through the board to find good locations to place a word
-
-public int[][] findValidLocations(Board b){
+// @return returns a 3D array where indicies 1&2 refer to location and 3 contains {maxHorizontal, minH, maxVertical, minV}
+public int[][][] findValidLocations(Board b){
+  int[][][] lengths = new int[15][15][4];
 for(int i = 0; i<15; i++){
   for(int j = 0; j<15; j++){
-  if(b.getSquare(new Location(i,j)) != isAlphabetical //or other symbols){
-    // Square is occupied
-    return 0;
+    Location current = new Location(i,j);
+  if(b.isOccupied(current)){
+    for(int k = 0; k<4; k++){
+      lengths[i][j][k] = 0;
     }
-    else{//check path 
-    
-    
-    if(// no neighbors within 7)
-     return 0;
-     }
-     else{
-     return path length
-
-
-
-
+  }
+   else{//check path 
+      int[] check = new int[2];
+     check = findEmptyPath(b,current,HORIZONTAL);
+      lengths[i][j][0] = check[0];
+      lengths[i][j][1] = check[1];
+      check = findEmptyPath(b,current,VERTICAL);
+      lengths[i][j][2] = check[0];
+      lengths[i][j][3] = check[1];
+    }
+  }
+  }
+  return lengths;
 }
+      public int[] findEmptyPath(board b, Location start, Location direction){
+       int min = -1; // minimum length needed to play off
+       int index = 0;
+       Location current = start;
+       while(index<9){
+         if(b.isOccupied(current) || !current.isOnBoard()){ // found a tile
+         break; 
+        }
+        if(min == -1 && (b.isOccupied(current.neighbor(direction.opposite) || b.isOccupied(current.antineighbor(direction.opposite))){
+         min = index+1;
+        }
+        index++;
+        current = current.neighbor(direction);
+       }
+           int[] values = new int[2];
+           if(min == -1 && index == 9){ //cannot play off
+            values[0] = 0;
+            values[1] = 0;
+           }
+           else if (min == -1 && index < 9){ // ends by ramming into a word, but nothing before then
+             values[0] = index;
+             values[1] = index;
+           }
+           else{
+             values[0] = index;
+             values[1] = min;
+           }
+           return values;
+      }
 
